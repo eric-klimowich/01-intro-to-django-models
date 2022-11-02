95bff28b65c8a3cf34c104df2ecc3470ac95e4c9
eric-klimowich <eklimowich@hotmail.com>
2022-11-02 16:33:31 -0400

    Add details page steps 1 and 2

--- starter-code/catcollector/main_app/views.py
+++ starter-code/catcollector/main_app/views.py
@@ -1,17 +1,5 @@
 from django.shortcuts import render
-
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+from .models import Cat
 
 # View functions
 
@@ -22,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
